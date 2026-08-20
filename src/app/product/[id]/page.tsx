b534497e19b787d39ec763b1a54ddfe7c23a6e80
bayzed123 <2.05858288e+08+bayzed123@users.noreply.github.com>
2026-08-20 12:20:35 +0000

    Move generateStaticParams to layout to avoid use client conflict

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,14 +1,4 @@
 "use client";
-export function generateStaticParams() {
-  return [
-    { id: '1' },
-    { id: '2' },
-    { id: '3' },
-    { id: '4' },
-    { id: '5' },
-    { id: '6' },
-  ]
-}
 
 import React, { useState } from "react";
 import Image from "next/image";
